42362ab728855699c0e315262a9e5127672841c5
ZackNemec <zacknemec@live.com>
2020-11-08 20:00:31 -0800

    mvp

--- project_router.js
+++ project_router.js
@@ -14,6 +14,60 @@ res.status(200).json(e)
 console.log(err);
 res.status(500).json({error: "error getting projects"})
 })
+});
+
+
+router.post('/', (req, res) => {
+    const newProject = req.body
+projects
+.insert(newProject)
+.then(() => {
+res.status(201).json({message: `project created`})
+})
+.catch(err => {
+res.status(500).json({error: "can not create project right now"})
+})
+});
+
+router.put("/:id", (req, res) => {
+const upProject = req.body;
+const {id} = req.params;
+projects
+.update(id, projects)
+.then(() => {
+res.status(200).json(upProject);
+})
+.catch(err => {
+console.log(err);
+res.status(500).json({
+    error: "The project information could not be modified."
+})
+})
+})
+
+router.delete("/:id", (req, res) => {
+    const id = req.params.id
+    projects
+    .remove(id)
+    .then(e => {
+    res.status(200).json(e);
+    })
+    .catch(err => {
+    console.log(err);
+    res.status(500).json({error: "The projects could not be removed"});
+    })
+    })
+
+router.get( '/:id/actions', (req, res) => {
+projects
+.getProjectActions(req.params.id)
+.then(e => {
+res.status(200).json(e)
+})
+.catch(err => {
+    console.log(err);
+    res.status(500).json({error: "could not get project actions"});
+    })
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require('express');
 const helmet = require("helmet");
 
 const projectRouter = require('./project_router.js');
+const actionRouter = require('./actions_router.js');
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use("/api/projects", projectRouter);
+server.use("/api/actions", actionRouter);
 
 server.get('/', (req, res) => {
 res.status(200).json({message: "api is up"});
